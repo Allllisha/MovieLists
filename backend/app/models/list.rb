fd3adafbc6001f7178b0b1e4117ad57d169bf3e6
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-04-06 23:36:22 +0900

    changed cors

--- backend/app/models/list.rb
+++ backend/app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy,  numericality: { in: 1..5 }
   has_many :movies, through: :bookmarks
   belongs_to :user, optional: true
   validates :name, presence: true, uniqueness: true
