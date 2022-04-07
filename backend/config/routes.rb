679c71a211552304f49c4b0581fc0d18b0312a89
Arisa Nemoto <Arisa@Arisa-no-MacBook-Air.local>
2022-04-07 22:53:23 +0900

    Created Frontend

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'auth/registrations'
+        registrations: 'api/v1/auth/registrations'
       }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
       resources :movies, only: [:index, :show]
       resources :movie_reviews
       resources :movie_genres
